732571d5bb93a08fc9aa958f7dbf95cf421244c5
jessicacliong <14395@coderacademy.edu.au>
2023-09-26 19:35:42 +1000

    feat: project init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import PokemonHook from './PokemonHook';
+
+function App() {
+  return (
+    <div className="App">
+      <PokemonHook />
+    </div>
+  );
+}
+
+export default App;
+
+export default App;
