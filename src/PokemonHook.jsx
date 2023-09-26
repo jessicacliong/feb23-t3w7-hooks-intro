50ce0fe86f5924bcd2d00ef404d65f84999fe172
jessicacliong <14395@coderacademy.edu.au>
2023-09-26 19:59:22 +1000

    feat: useEffect for componentDidMount

--- src/PokemonHook.jsx
+++ src/PokemonHook.jsx
@@ -1,10 +1,31 @@
-
+import { useEffect, useState } from "react";
 
 export default function PokemonHook(){
 
+
+	//  [variable, function to set variable] = useState(initial data);
+	let [pokemon, setPokemon] = useState({});
+
+	let [someExampleState, setSomeExampleState] = useState("hello world");
+	
+  // Don't do setState in top level of function, use at a lower level
+	// setPokemon("whatever we want");
+
+  // runs at the start or on the first render of this component
+	// useEffect((), []) is equivalent to componentDidMount
+	// because the dependency array is empty 
+  useEffect(() => {
+    setPokemon({name: "Pikachu"});
+  }, []);
+
+  function someFunction(){
+    console.log("blah blah")
+  }
+
 	return (
 		<div>
-			<h1>Feature not yet implemented.</h1>
+			<h1>{someExampleState}</h1>
+			<h1>{pokemon.name}</h1>
 		</div>
 	)
 }
